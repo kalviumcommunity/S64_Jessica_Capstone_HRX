3a16e26502a89eb40cd040efde130166e3f205db
jessicaagarwal <jessica.agarwal.s64@kalvium.community>
2025-04-11 16:44:20 +0530

    schema updated

---
+++ backend/models/User.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a name']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please add an email'],
+    unique: true
+  },
+  password: {
+    type: String,
+    required: [true, 'Please add a password'],
+    minlength: 6,
+    select: false
+  },
+  role: {
+    type: String,
+    enum: ['employee', 'hr'],
+    default: 'employee'
+  },
+  department: String,
+  jobTitle: String,
+  salary: Number,
+  bankDetails: {
+    accountNumber: String,
+    ifscCode: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+export default User;
